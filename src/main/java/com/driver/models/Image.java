0bb4cc87834d254dd40f068a6644b2fc1ae3a3bc
Anandu-Kp <anandukp06@gmail.com>
2023-02-21 15:57:19 +0530

    commit

--- src/main/java/com/driver/models/Image.java
+++ src/main/java/com/driver/models/Image.java
@@ -3,35 +3,49 @@ package com.driver.models;
 import javax.persistence.*;
 
 @Entity
-@Table(name ="image")
+@Table(name = "image")
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    private String descriptions;
+
+    private String description;
     private String dimensions;
 
     @ManyToOne
     @JoinColumn
     Blog blog;
 
-    public Image() {
+    public Blog getBlog() {
+        return blog;
     }
 
-    public int getId() {
-        return id;
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
+    public Image() {
+
+    }
+
+    public Image(String description, String dimensions) {
+        this.description = description;
+        this.dimensions = dimensions;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getDescriptions() {
-        return descriptions;
+    public int getId() {
+        return id;
     }
 
-    public void setDescriptions(String descriptions) {
-        this.descriptions = descriptions;
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getDimensions() {
@@ -40,13 +54,5 @@ public class Image {
 
     public void setDimensions(String dimensions) {
         this.dimensions = dimensions;
-    }
-
-    public Blog getBlog() {
-        return blog;
-    }
-
-    public void setBlog(Blog blog) {
-        this.blog = blog;
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -19,9 +19,9 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-        Image image=new Image();
-        image.setDescriptions(description);
-        image.setDimensions(dimensions);
+        Image image=new Image(description,dimensions);
+//        image.setDescription(description);
+//        image.setDimensions(dimensions);
         Blog blog=blogRepository2.findById(blogId).get();
         image.setBlog(blog);
 
